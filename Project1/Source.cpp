66d6935562a889baf021da9e6b7e4a3c07f37f48
GalicaAnna <anjagalica@gmail.com>
2026-03-26 14:37:52 +0200

    CPP learning

---
+++ Project1/Source.cpp
@@ -0,0 +1,13 @@
+#include"iostream"
+#include"Windows.h"
+
+int main()
+{
+	SetConsoleCP(65001);
+	SetConsoleOutputCP(65001);
+	using namespace std;
+	
+	cout << "Привіт,гарний і чудовий ранок!";
+	return 0;
+
+}
